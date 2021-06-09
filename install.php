ddcb69ae0b1a10048ea579876718b2f051733594
Alexander Walther <github.com@alexplus.de>
2021-06-09 15:58:28 +0200

    remove outdated copyright notice
    Das Original-Addon ist unter MIT-Lizenz. Ein Verweis genügt.

--- install.php
+++ install.php
@@ -2,11 +2,6 @@
 
 /**
  * This file is part of the Url package.
- *
- * @author (c) Thomas Blum <thomas@addoff.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 \rex_sql_table::get(
